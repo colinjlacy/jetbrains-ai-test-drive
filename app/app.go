a29073dfdc4d796e3fa46bdca8791719c9600b0b
Colin Lacy <colin.lacy@meraki.net>
2023-12-27 06:15:53 -0500

    ends part 2

--- app/app.go
+++ app/app.go
@@ -1,3 +1,29 @@
 package app
 
-func Start() {}
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Start() {
+	r := gin.Default()
+
+	// THE JBAI PUT THIS HERE, NOT ME
+	registerHealthEndpoint(r)
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello, world",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func registerHealthEndpoint(engine *gin.Engine) {
+	engine.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+}
--- app/app_test.go
+++ app/app_test.go
@@ -1,1 +1,56 @@
 package app
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStart(t *testing.T) {
+	t.Run("GET root path", func(t *testing.T) {
+		gin.SetMode(gin.TestMode)
+
+		router := gin.Default()
+
+		router.GET("/", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "hello, world",
+			})
+		})
+
+		request, err := http.NewRequest(http.MethodGet, "/", nil)
+		if err != nil {
+			t.Fatalf("Couldn't create request: %s\n", err)
+		}
+
+		respRec := httptest.NewRecorder()
+		router.ServeHTTP(respRec, request)
+
+		assert.Equal(t, http.StatusOK, respRec.Code)
+		assert.Equal(t, `{"message":"hello, world"}`, respRec.Body.String())
+	})
+}
+
+func TestRegisterHealthEndpoint(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+
+	registerHealthEndpoint(r)
+
+	req, _ := http.NewRequest("GET", "/health", nil)
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+
+	if resp.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, resp.Code)
+	}
+
+	expectedResponse := `{"status":"ok"}`
+	body := resp.Body.String()
+	if body != expectedResponse {
+		t.Errorf("Expected body '%s', but got '%s'", expectedResponse, body)
+	}
+}
